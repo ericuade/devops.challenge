0fa7b749de7f7891af42affb180f480d6154a46c
eogieglo2 <eogieglo@uade.edu.ar>
2023-03-23 12:31:58 -0300

    ADD ejercicio 2 python

---
+++ Python extra/ejercicio2.py
@@ -0,0 +1,33 @@
+# Manejo de datos de tipo números, operadores
+
+"""
+    Ejercicio 2. Operadores matemáticos en Python. Calcular el valor de las siguientes expresiones aritméticas, 
+    respetando el orden establecido.
+    a. 12 * (4 + 4) / 2 (48)
+    b. (24 / 2) ** 2 (144.0)
+    c. 9 ** 2 (81)
+    d. √81 (9)
+    e. 10 / 3 (3.333333…)
+    f. 10 // 3 (3)
+    g. 12 % 5 (2)
+    Nota: la raíz cuadrada de un número puede calcularse elevando el número a la potencia ½ o 0,5. Por ejemplo, 
+    raíz cuadrada de 16 se puede expresar en Python como 16 ** 0.5.
+"""
+
+print("Manejo de datos de tipo números, operadores")
+print("-------------------------------------------")
+ej_a = 12 * (4 + 4) / 2
+print("El resultado de A. = ", ej_a)
+ej_b = (24 / 2) ** 2
+print("El resultado de B. = ", ej_b)
+ej_c = 9 ** 2
+print("El resultado de C =", ej_c)
+ej_d = 81 ** 0.5
+print("El resultado de D =", ej_d)
+ej_e = 10 / 3
+print("El resultado de E =", ej_e)
+ej_f = 10 // 3
+print("El resultado de F =", ej_f)
+ej_g = 12 % 5
+print("El resultado de G =", ej_g)
+

1cd5c048f62014379f3bc7c6586e9f0c72b156ca
eogieglo2 <eogieglo@uade.edu.ar>
2023-03-27 13:23:20 -0300

    Fix: syntax

--- test_homework.py
+++ test_homework.py
@@ -12,6 +12,7 @@ class TestingMethods(unittest.TestCase):
   def test_divide(self):
     self.assertEqual(divide(4, 2), 2, "La division NO es correcta")
 
-if __name__ == "__main__":  
-    unittest.main()
+
+if __name__ == "__main__":
+  unittest.main()
       

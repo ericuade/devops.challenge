551a61be852167e9dedd24ed2116e0a97d60d38d
eogieglo2 <eogieglo@uade.edu.ar>
2023-04-01 07:37:08 -0300

    Type hints in FastAPI examples

--- Python/api/type_hints.py
+++ Python/api/type_hints.py
@@ -9,3 +9,16 @@ print(type(my_string_variable))
 my_string_variable = 5
 print(my_string_variable)
 print(type(my_string_variable))
+
+# asignamos a proposito un tipo int con un valor string, para mostrar que el editor no lo toma como invalido
+my_typed_variable: int = "My tiped variable"
+my_typed_variable.bit_count() # Va a tirar error en tiempo de ejecucion, podemos usar los metodos de int aunque el valor sea un string en el editor. OJO!
+print(my_typed_variable) 
+print(type(my_typed_variable))
+
+my_typed_variable = 5
+print(my_typed_variable)
+print(type(my_typed_variable))
+
+
+

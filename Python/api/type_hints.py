5aadfddb223e1ff466c59bb4bbd979a4b6e1097a
eogieglo2 <eogieglo@uade.edu.ar>
2023-04-01 07:23:49 -0300

    Examples type dinamyc

---
+++ Python/api/type_hints.py
@@ -0,0 +1,11 @@
+# Estamos trabajando con FastAPI
+# Type Hints , mostrando ejemplos de tipado dinamico
+
+my_string_variable = "My string Variable"
+print(my_string_variable)
+print(type(my_string_variable))
+
+# Le asignamos valor numerico
+my_string_variable = 5
+print(my_string_variable)
+print(type(my_string_variable))

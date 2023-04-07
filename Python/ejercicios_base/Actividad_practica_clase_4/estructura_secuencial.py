295192085f8d85902487cdcd7039836b79de17bf
eogieglo2 <eogieglo@uade.edu.ar>
2023-04-07 19:34:38 -0300

    new file:   Python/ejercicios_base/Actividad_practica_clase_4/estructura_secuencial.py

---
+++ Python/ejercicios_base/Actividad_practica_clase_4/estructura_secuencial.py
@@ -0,0 +1,15 @@
+# Estructura secuencial
+
+# Ejercicio 1: 
+
+"""
+    Calcular el promedio de las notas que obtiene un 
+    alumno al rendir los dos parciales. 
+"""
+
+# Ejercicio 2:
+
+"""
+     Leer una medida en metros e imprimir esta medida 
+     expresada en centímetros y kilómetros.
+"""

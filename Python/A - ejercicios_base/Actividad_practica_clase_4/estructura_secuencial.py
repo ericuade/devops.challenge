5eb609e1f53a0bb17fc5622b02b7d90bfb551060
eogieglo2 <eogieglo@uade.edu.ar>
2023-04-11 19:21:46 -0300

    ADD estructura secuencial

--- Python/A - ejercicios_base/Actividad_practica_clase_4/estructura_secuencial.py
+++ Python/A - ejercicios_base/Actividad_practica_clase_4/estructura_secuencial.py
@@ -7,9 +7,25 @@
     alumno al rendir los dos parciales. 
 """
 
+print("Ejercicio 1")
+print("-----------")
+nota1 = (int(input("Ingrese nota 1: ")))
+nota2 = (int(input("Ingrese nota 2: ")))
+promedio = (nota1 + nota2) / 2
+print("El promedio de las notas es: ", promedio)
+print("")
+
 # Ejercicio 2:
 
 """
      Leer una medida en metros e imprimir esta medida 
      expresada en centímetros y kilómetros.
 """
+
+print("Ejercicio 2 - Ingrese medida en Metros y devuelve el programa centrimetros y kilometros")
+print("---------------------------------------------------------------------------------------")
+medida_metros = int(input("Ingrese la medida en Metros: "))
+print("")
+print("El resultado en cm es: ", medida_metros * 100, " cm")
+print("El resultado en km es: ", medida_metros / 1000, " km")
+print("")
